f6804a7d0963c087eab9ceb3c675538afddae4dd
r0p3_laptop <ropefalora@gmail.com>
2021-04-08 00:30:26 +0000

    commit to continue on mi desktop computer

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ fn main() {
     let app = App::from_yaml(yaml)
         .arg(
             clap::Arg::with_name("nuevo")
+                .short("n")
                 .takes_value(true)
-                .last(true))
+                // .last(true)
+                .use_delimiter(true)
+                .value_delimiter(",")
+            )
         .get_matches();
     println!("Address: {}", app.value_of("address").unwrap());
     println!("Threads: {}", app.value_of("threads").unwrap());
     println!("Ports: {}", app.value_of("ports").unwrap());
+    println!("Ports: {:?}", app.values_of("ports").unwrap().collect::<Vec<&str>>());
+
+    // println!("Nuevo: {:?}", app.values_of("nuevo").unwrap().collect::<Vec<&str>>());
 
     // println!("addr: {:?}", app.subcommand_matches("address").unwrap());
     println!("MARIKONG");
