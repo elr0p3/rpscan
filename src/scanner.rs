9d31c08f307038435c1c2b38a9b84f05576574d6
elr0p3 <ropefalora@gmail.com>
2021-04-10 02:24:41 +0200

    Able to scan list of individual ports

--- src/scanner.rs
+++ src/scanner.rs
@@ -28,7 +28,7 @@ pub struct Scanner {
     threads: u8,
     low_port: u16,
     high_port: u16,
-    port_list: HashSet<u16>,
+    port_list: Vec<u16>,
 
     open_ports: HashSet<u16>,
 }
@@ -49,26 +49,27 @@ impl Scanner {
         };
         let mut low_port: u16 = 0;
         let mut high_port: u16 = 0;
-        let mut port_list: HashSet<u16> = HashSet::new();
+        let mut port_hash: HashSet<u16> = HashSet::new();
         let ports_len = ports.len();
 
         if ports_len == 1 && ports[0].contains("-") {
             if let Err(err) = Self::range_ports(&mut threads, &mut low_port, &mut high_port, ports) {
                 return Err(err);
             };
+            threads = if ((high_port - low_port) as u8) < threads { ports_len as u8 } else { threads };
 
             Ok(Self{
                 address,
                 threads,
                 low_port,
                 high_port,
-                port_list: HashSet::new(),
+                port_list: Vec::new(),
                 open_ports: HashSet::new(),
             })
-        } else if ports_len > 1 {
+        } else if ports_len > 1 || !ports[0].contains("-") {
             for port in ports {
                 let port: u16 = port.parse().unwrap();
-                port_list.insert(port);
+                port_hash.insert(port);
             }
             threads = if (ports_len as u8) < threads { ports_len as u8 } else { threads };
 
@@ -77,7 +78,7 @@ impl Scanner {
                 threads,
                 low_port,
                 high_port,
-                port_list,
+                port_list: port_hash.into_iter().collect(),
                 open_ports: HashSet::new(),
             })
         } else {
@@ -110,29 +111,51 @@ impl Scanner {
         let sc = self.clone();
         let sc = Arc::new(sc);
 
-        for i in 0..self.threads {
-            let tx = tx.clone();
-            let sc_clone = Arc::clone(&sc);
-
-            if self.port_list.len() == 0 {
-                thread::spawn(move|| {
-                    Scanner::connect_host_range(
-                        tx, sc_clone.address, sc_clone.low_port, sc_clone.high_port, sc_clone.threads, i
-                    );
-                });
-            } else {
-                thread::spawn(move|| {
-                    Scanner::connect_host_list(tx);
-                });
-            }
+        if self.port_list.len() == 0 {
+            Self::scan_range(sc, tx);
+        } else {
+            Self::scan_list(sc, tx);
         }
 
-        drop(tx);
+        // drop(tx);
         for port in rx {
             self.open_ports.insert(port);
         }
 
         println!("Open Ports for {}:\n{:?}", self.address, self.open_ports);
+    }
+
+    fn scan_range (sc: Arc<Self>, tx: Sender<u16>) {
+        for i in 0..sc.threads {
+            let tx = tx.clone();
+            let sc_clone = Arc::clone(&sc);
+            thread::spawn(move|| {
+                Scanner::connect_host_range(
+                    tx, sc_clone.address, sc_clone.low_port, sc_clone.high_port, sc_clone.threads, i
+                );
+            });
+        }
+    }
+
+    fn scan_list (sc: Arc<Self>, tx: Sender<u16>) {
+        // let divide = ((sc.port_list.len() as f32) / sc.threads as f32).floor() as usize;
+        // let divide = ((sc.port_list.len() as f32) / sc.threads as f32).ceil() as usize;
+        let mut slices: Vec<Vec<u16>> = vec![Vec::new(); sc.threads as usize];
+
+        for i in 0..sc.port_list.len() {
+            let port = sc.port_list[i];
+            slices[i % sc.threads as usize].push(port);
+        }
+
+        for slice in slices {
+            let tx = tx.clone();
+            let sc_clone = Arc::clone(&sc);
+            thread::spawn(move|| {
+                Scanner::connect_host_list(
+                    tx, sc_clone.address, &slice
+                );
+            });
+        }
     }
 
     fn connect_host_range (tx: Sender<u16>, addr: Ipv4Addr, start: u16, end: u16, threads: u8, position: u8) {
@@ -156,8 +179,18 @@ impl Scanner {
         }
     }
 
-    fn connect_host_list (tx: Sender<u16>) {
-        
+    fn connect_host_list (tx: Sender<u16>, addr: Ipv4Addr, port_slice: &[u16]) {
+        for port in port_slice {
+            match TcpStream::connect_timeout(
+                &SocketAddr::new(IpAddr::V4(addr), *port), Duration::from_millis(10)
+                ) {
+                    Ok(_) => {
+                        println!("- {}", *port);
+                        tx.send(*port).unwrap();
+                    },
+                    Err(_) => {},
+            }
+        }
     }
 
 }
